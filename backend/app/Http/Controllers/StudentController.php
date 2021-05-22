a17b4810b7f6c1d8465db52316e95aadb7d75177
noza0727 <shahnoza072@gmail.com>
2021-05-23 04:28:42 +0500

    feat: getting active quizzes

--- backend/app/Http/Controllers/StudentController.php
+++ backend/app/Http/Controllers/StudentController.php
@@ -3,20 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Quiz;
-use App\Student;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
 class StudentController extends Controller
 {
-
-
     public static function getCompletedQuizzes($id): array
     {
         $now = Carbon::now();
         $current_time = Carbon::createFromFormat('Y-m-d H:i:s', $now, 'UTC')
             ->setTimezone('Asia/Tashkent');
+
         $student_quizzes = DB::table('student_quiz')->where('student_id', $id)->get();
         $quiz_array = array();
         foreach ($student_quizzes as $student_quiz){
@@ -36,16 +33,23 @@ class StudentController extends Controller
 
     public static function getActiveQuizzes($id): array
     {
+        $now = Carbon::now();
+        $current_time = Carbon::createFromFormat('Y-m-d H:i:s', $now, 'UTC')
+            ->setTimezone('Asia/Tashkent');
+
         $student_quizzes = DB::table('student_quiz')->where('student_id', $id)->get();
         $quiz_array = array();
         foreach ($student_quizzes as $student_quiz){
             $q_id = $student_quiz->quiz_id;
             $quizzes = Quiz::where('id', $q_id)->get();
             foreach ($quizzes as $quiz) {
-                array_push($quiz_array, $quiz);
+                $created_at = $quiz->created_at;
+                $expire = $created_at->addMinutes($quiz->duration);
+                if($expire > $current_time){
+                    array_push($quiz_array, $quiz);
+                }
             }
         }
         return $quiz_array;
     }
-
 }
